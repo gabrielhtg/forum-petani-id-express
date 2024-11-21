3d745d96e3af3d34455e50f468d572036b3baa1a
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-11-22 06:21:57 +0700

    menyesuaikan jwt

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,19 +1,19 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+  const token = req.header("authorization")?.split(" ")[1];
 
-    if (!token) {
-        return res.status(401).json({ message: 'Akses ditolak. Token tidak ada.' });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    try {
-        req.user = jwt.verify(token, process.env.JWT_SECRET);
-        next();
-    } catch (error) {
-        return res.status(400).json({ message: 'Token tidak valid.' });
-    }
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid Token" });
+  }
 };
 
 module.exports = authMiddleware;
