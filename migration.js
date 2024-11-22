b903213a5d0131f35fd3add59fe542c4bc6e3672
Gerry Bukit <ifs21055@students.del.ac.id>
2024-11-22 20:41:26 +0700

    tag migration

--- migration.js
+++ migration.js
@@ -1,24 +1,24 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 };
 
 const queries = [
-    `CREATE TABLE IF NOT EXISTS users (
+  `CREATE TABLE IF NOT EXISTS users (
         id INT PRIMARY KEY AUTO_INCREMENT,
         name varchar(100) NOT NULL,
         pekerjaan varchar(100) not null,
@@ -32,7 +32,7 @@ const queries = [
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )`,
-    `
+  `
     CREATE TABLE Content (
     contentId INTEGER PRIMARY KEY AUTO_INCREMENT,
     uploaderId VARCHAR(255) NOT NULL,            
@@ -40,25 +40,30 @@ const queries = [
     gambar BLOB,                                  
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP                         
-);
-    `
+)`,
+  `CREATE TABLE IF NOT EXISTS Tag_Produk (
+        tagId VARCHAR(255) INTEGER PRIMARY KEY AUTO_INCREMENT,
+        uploaderId VARCHAR(255) NOT NULL,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )`,
 ];
 
 (async () => {
-    const connection = await mysql.createConnection(config);
+  const connection = await mysql.createConnection(config);
 
-    try {
-        console.log('Migration dimulai!');
+  try {
+    console.log("Migration dimulai!");
 
-        for (const e of queries) {
-            await connection.query(e)
-            console.log(`Query berhasil dieksekusi: ${e}\n`)
-        }
-
-        console.log('Migrasi berhasil!');
-    } catch (error) {
-        console.error('Terjadi kesalahan saat migrasi:', error);
-    } finally {
-        await connection.end();
+    for (const e of queries) {
+      await connection.query(e);
+      console.log(`Query berhasil dieksekusi: ${e}\n`);
     }
+
+    console.log("Migrasi berhasil!");
+  } catch (error) {
+    console.error("Terjadi kesalahan saat migrasi:", error);
+  } finally {
+    await connection.end();
+  }
 })();
